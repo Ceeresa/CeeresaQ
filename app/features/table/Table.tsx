273baf44ec964d0d0fc3f82cd99207f3de15a546
Davide Pastore <d.pastore@logispin.com>
2020-07-14 17:03:26 +0200

    Add first draft of table in Table.tsx

--- app/features/table/Table.tsx
+++ app/features/table/Table.tsx
@@ -1,71 +1,42 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import styles from './Table.css';
-import routes from '../../constants/routes.json';
-import {
-  increment,
-  decrement,
-  incrementIfOdd,
-  incrementAsync,
-  selectCount,
-} from './tableSlice';
+import MaterialTable from 'material-table';
 
 export default function Table() {
-  const dispatch = useDispatch();
-  const value = useSelector(selectCount);
   return (
-    <div>
-      <div className={styles.backButton} data-tid="backButton">
-        <Link to={routes.HOME}>
-          <i className="fa fa-arrow-left fa-3x" />
-        </Link>
-      </div>
-      <div className={`counter ${styles.counter}`} data-tid="counter">
-        {value}
-      </div>
-      <div className={styles.btnGroup}>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            dispatch(increment());
-          }}
-          data-tclass="btn"
-          type="button"
-        >
-          <i className="fa fa-plus" />
-        </button>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            dispatch(decrement());
-          }}
-          data-tclass="btn"
-          type="button"
-        >
-          <i className="fa fa-minus" />
-        </button>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            dispatch(incrementIfOdd());
-          }}
-          data-tclass="btn"
-          type="button"
-        >
-          odd
-        </button>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            dispatch(incrementAsync());
-          }}
-          data-tclass="btn"
-          type="button"
-        >
-          async
-        </button>
-      </div>
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        columns={[
+          { title: 'Id', field: 'id', type: 'string' },
+          { title: 'Title', field: 'title', type: 'string' },
+          { title: 'Description', field: 'description', type: 'string' },
+          {
+            title: 'Tags',
+            field: 'tags',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          },
+          { title: 'SQL', field: 'query', type: 'string' },
+        ]}
+        data={[
+          {
+            id: 'Mehmet',
+            title: 'Baran',
+            description: 'Description 1',
+            tags: 1987,
+            query: 'SELECT * FROM test',
+          },
+          {
+            id: 'Gigi',
+            title: 'Valli',
+            description: 'Description 2',
+            tags: 1999,
+            query: 'SELECT * FROM just_another_test',
+          },
+        ]}
+        options={{
+          filtering: true,
+        }}
+        title="Demo Title"
+      />
     </div>
   );
 }
